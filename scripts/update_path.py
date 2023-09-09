4d15b6d7288f4521af9d25f65588a288a8580a53
2010johnlee <2010johnlee@gmail.com>
2023-09-09 13:58:33 -0700

    Update update_path.py

--- scripts/update_path.py
+++ scripts/update_path.py
@@ -1,21 +1,38 @@
+import os
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
 
-# Replace 'your-credentials.json' with the path to your Google Cloud credentials JSON file
-credentials_file = 'credentials.json'
-file_path = '/path/to/your/file/on/google/drive/filename.ext'
+# Replace with the path to your credentials JSON file.
+credentials_file = 'path/to/your/credentials.json'
 
-# Load credentials from the JSON file
+# Create a service account credentials object
 credentials = service_account.Credentials.from_service_account_file(
-    credentials_file, scopes=['https://www.googleapis.com/auth/drive.readonly'])
+    credentials_file
+)
 
-# Create a Drive API service
+# Create a Google Drive API service
 drive_service = build('drive', 'v3', credentials=credentials)
 
-# Search for the file
-results = drive_service.files().list(q=f"name='{file_path}'").execute()
-files = results.get('files', [])
+def list_files_in_folder(folder_id):
+    try:
+        results = drive_service.files().list(
+            q=f"'{folder_id}' in parents",
+            fields="nextPageToken, files(id, name)"
+        ).execute()
 
+        files = results.get('files', [])
+        return files
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return []
+
+def main():
+    # Replace with the ID of the folder you want to list files from.
+    folder_id = 'YOUR_FOLDER_ID_HERE'
+
+    files = list_files_in_folder(folder_id)
+    
 if not files:
     print(f'File "{file_path}" not found on Google Drive.')
 else:
@@ -25,3 +42,6 @@ else:
     # Write the URL to a Markdown file
     with open('output.md', 'w') as md_file:
         md_file.write(f'[Link Text]({file_url})')
+
+if __name__ == '__main__':
+    main()
